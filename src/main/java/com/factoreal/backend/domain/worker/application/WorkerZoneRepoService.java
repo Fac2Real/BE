fee80bf6a14e2a1945779beaa561e34e64599793
Juyoung Jeong <wdd1016@gmail.com>
2025-05-31 10:56:49 +0900

    Revert "feat | sprint3 | FRB-210 | Wearable 상태 제공 로직 수정 | 정주영"
    This reverts commit fc76f30dbc5077f27559136166b9f4ba11126ec2.

--- src/main/java/com/factoreal/backend/domain/worker/application/WorkerZoneRepoService.java
+++ src/main/java/com/factoreal/backend/domain/worker/application/WorkerZoneRepoService.java
@@ -46,8 +46,4 @@ public class WorkerZoneRepoService {
     public WorkerZone save(WorkerZone workerZone) {
         return workerZoneRepository.save(workerZone);
     }
-
-    public List<WorkerZone> findByWorker_WorkerId(String workerId) {
-        return workerZoneRepository.findByWorker_WorkerId(workerId);
-    }
 }
