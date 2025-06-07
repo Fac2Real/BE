492f03d41c0d3bdf7aed4adfde37f64d7be1453d
minnnseokk <msjeong991029@gmail.com>
2025-06-07 23:03:27 +0900

    feature | sprint3 | FRB-222 | 작업자 생성 중복시 409 에러 발생로직 추가 | 정민석

--- src/main/java/com/factoreal/backend/domain/worker/dao/WorkerRepository.java
+++ src/main/java/com/factoreal/backend/domain/worker/dao/WorkerRepository.java
@@ -2,10 +2,18 @@ package com.factoreal.backend.domain.worker.dao;
 
 import com.factoreal.backend.domain.worker.entity.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface WorkerRepository extends JpaRepository<Worker, String> {
     List<Worker> findByWorkerIdIn(Collection<String> ids);
+
+    /**
+     * 중복 작업자 체크용 메서드
+     */
+    boolean existsByWorkerId(String workerId);
+    boolean existsByPhoneNumber(String phoneNumber);
+
 }
--- src/main/java/com/factoreal/backend/domain/worker/dao/WorkerZoneRepository.java
+++ src/main/java/com/factoreal/backend/domain/worker/dao/WorkerZoneRepository.java
@@ -20,4 +20,5 @@ public interface WorkerZoneRepository extends JpaRepository<WorkerZone, WorkerZo
 
     // 특정 작업자가 담당자로 있는 공간 조회
     Optional<WorkerZone> findByWorkerWorkerIdAndManageYnIsTrue(String workerId);
+
 }
