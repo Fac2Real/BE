8d94b0c9ae26e6ae6c7d95c39430bef421d41fef
minnnseokk <msjeong991029@gmail.com>
2025-06-08 19:21:00 +0900

    feature | sprint3 | FRB-224 | getZoneItems 한글 센서 타입 속성 추가 | 정민석

--- src/main/java/com/factoreal/backend/messaging/kafka/strategy/enums/SensorType.java
+++ src/main/java/com/factoreal/backend/messaging/kafka/strategy/enums/SensorType.java
@@ -1,14 +1,21 @@
 package com.factoreal.backend.messaging.kafka.strategy.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum SensorType {
-    current,
-    dust,
-    temp,
-    humid,
-    vibration,
-    power,
-    pressure,
-    voc;
+    temp("온도 센서"),
+    humid("습도 센서"),
+    vibration("진동 센서"),
+    power("전력 센서"),
+    current("전류 센서"),
+    pressure("압력 센서"),
+    dust("먼지 센서"),
+    voc("휘발성유기화합물 센서");
+
+    private final String koName;
     public static SensorType getSensorType(String sensorType){
         return SensorType.valueOf(sensorType);
     }
